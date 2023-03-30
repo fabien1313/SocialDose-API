f7146e3a362e12510cf2c9daf5d927b48316d588
fabien moreno <fabienmoreno1331@yahoo.com>
2023-03-30 15:54:41 -0500

    Serving is starting up

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const express = require('express'); // Import express
-const db = require('./config/connection'); // Import connection.js
-const routes = require('./controllers'); // Import routes
+const express = require('express'); 
+const db = require('./config/connection'); 
+const routes = require('./controllers'); 
 
-const PORT = process.env.PORT || 3001; // Define port
+const PORT = process.env.PORT || 3001; 
+const app = express(); 
+
+app.use(express.urlencoded({ extended: true })); // Middleware parsing incoming HTTP POST requests where the body contains data encoded in the URL-encoded format. True - allows for parsing of nested objects.
+app.use(express.json()); //Middleware parsing HTTP request bodies that use JSON format.
+app.use(routes);
+
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
